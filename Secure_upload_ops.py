83dc93fea43518602d982a0a30d15f765446e537
pmamin4 <70705672+pmamin4@users.noreply.github.com>
2021-02-20 00:13:45 -0500

    Update Secure_upload_ops.py

--- Secure_upload_ops.py
+++ Secure_upload_ops.py
@@ -1,4 +1,11 @@
-// uploading files
+Class upload
+
+  type()
+  form()
+  viewer()
+  editor()
+  commentator()
+  
 
 def upload(file):
   
@@ -9,15 +16,15 @@ def upload(file):
     log(file)
     return 'saved'
 
-def convert_to_form(file, form):
+def convert_to_type(file, new_type):
 
-  if form is invalid:
-    return 'invalid'
-  elif filetype is invalid:
-    return 'invalid'
+  if new_type is invalid:
+    return 'invalid new type'
+  elif file.type() is invalid:
+    return 'invalid file'
   else:
-    file_in_form = newfile
-    return upload(newfile)
+    file.type() = new_type
+    return 'type updated'
 
 
 def enable_viewing(file, viewer):
@@ -29,20 +36,21 @@ def enable_viewing(file, viewer):
     return 'invalid email'
 
   else:
+      file.viewer = viewer
       return "viewer enabled"
 
 
 def enable_editing(file, editor):
 
   enable_viewing(file,editor)
-  enable(editor)
+  file.editor = editor
   return 'editor enabled'
 
 
 def enable_commenting(file, commentator):
    
    enable_viewing(file,commentator)
-   enable(commentator)
+   file.commentator = commentator
    return 'commentator enabled'
 
 
